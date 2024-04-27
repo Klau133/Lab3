42ee2c2dff430c4ecbc5e2d84ac90279977b8b6a
Klaudia <dsw52731@student.dsw.edu.pl>
2024-04-27 11:45:19 +0200

    c1

---
+++ komendy.sh
@@ -0,0 +1,20 @@
+git commit
+git checkout <nazwa-gałęzi>
+git checkout -b <nazwa-gałęzi>
+git branch <nazwa>
+git merge <nazwa>
+git rebase
+^
+~<num>
+git branch -f main HEAD~3
+git revert
+git reset
+git branch pushed
+git cherry-pick <Commit1> <Commit2> <...>
+git rebase -i overHere --solution-ordering C3,C5,C4
+git rebase caption main
+git commit --amend
+git tag <nazwa_tag> <commit>
+git describe <ref>
+git bisect
+<tag>_<numCommits>_g<hash>
